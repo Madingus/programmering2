ad0847e688439cea671abf53e92e10fcd3666228
Alexander Andreasson <aleand0110@skynet.halmstad.se>
2019-09-09 09:41:20 +0200

    start of 1 dot 4

--- övning1_4/övning1_4/Form1.Designer.cs
+++ övning1_4/övning1_4/Form1.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cbxFet = new System.Windows.Forms.CheckBox();
+            this.cbxKursiv = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(240, 113);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "What is love? Baby dont hurt me, dont hurt me, no more. dun dun dun dundundun dun" +
+    " dun dun dundundun dun dun dundunudn dundu";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // cbxFet
+            // 
+            this.cbxFet.AutoSize = true;
+            this.cbxFet.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxFet.Location = new System.Drawing.Point(129, 288);
+            this.cbxFet.Name = "cbxFet";
+            this.cbxFet.Size = new System.Drawing.Size(44, 17);
+            this.cbxFet.TabIndex = 2;
+            this.cbxFet.Text = "Fet";
+            this.cbxFet.UseVisualStyleBackColor = true;
+            // 
+            // cbxKursiv
+            // 
+            this.cbxKursiv.AutoSize = true;
+            this.cbxKursiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxKursiv.Location = new System.Drawing.Point(129, 339);
+            this.cbxKursiv.Name = "cbxKursiv";
+            this.cbxKursiv.Size = new System.Drawing.Size(55, 17);
+            this.cbxKursiv.TabIndex = 3;
+            this.cbxKursiv.Text = "Kursiv";
+            this.cbxKursiv.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(237, 229);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Teckensnitt";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Textstorlek";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.cbxKursiv);
+            this.Controls.Add(this.cbxFet);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox cbxFet;
+        private System.Windows.Forms.CheckBox cbxKursiv;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
