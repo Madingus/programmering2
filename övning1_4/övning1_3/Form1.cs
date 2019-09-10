968b86733ab40f865f18690887f242e2008aaec4
Alexander Andreasson <aleand0110@skynet.halmstad.se>
2019-09-10 15:12:46 +0200

    övning1_3

---
+++ övning1_4/övning1_3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace övning1_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            PictureBox[,] bilder = new PictureBox[5, 5];
+
+            for (int r = 0; r < 5; r++)
+            {
+                for (int c = 0; c < 5; r++)
+                {
+
+
+
+                    bilder[r, c] = new PictureBox()
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+    }
+}
