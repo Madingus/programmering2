f6019797fbfe55f8605bfd307cf1057346591773
Alexander Andreasson <aleand0110@skynet.halmstad.se>
2019-12-02 09:41:06 +0100

    övning 2.1 (bok, media thingy) in progress

---
+++ övning 2.1 (media, bok klasser)/övning 2.1 (media, bok klasser)/Film.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace övning_2._1__media__bok_klasser_
+{
+    class Film
+    {
+    }
+}
---
+++ övning 2.1 (media, bok klasser)/övning 2.1 (media, bok klasser)/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace övning_2._1__media__bok_klasser_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxBokTitel = new System.Windows.Forms.TextBox();
+            this.btnBok = new System.Windows.Forms.Button();
+            this.lbxList = new System.Windows.Forms.ListBox();
+            this.tbxBokSidor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxLjudSpeltid = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLjudspår = new System.Windows.Forms.Button();
+            this.tbxLjudTitel = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxFilmSpeltid = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbxFilmTitel = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxFilmUpplösning = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbxBokSidor);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnBok);
+            this.groupBox1.Controls.Add(this.tbxBokTitel);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(43, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 159);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bok";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbxLjudSpeltid);
+            this.groupBox2.Controls.Add(this.btnLjudspår);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tbxLjudTitel);
+            this.groupBox2.Location = new System.Drawing.Point(298, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(222, 159);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ljudspår";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbxFilmUpplösning);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.tbxFilmSpeltid);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.tbxFilmTitel);
+            this.groupBox3.Location = new System.Drawing.Point(550, 35);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(222, 159);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Film";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "titel";
+            // 
+            // tbxBokTitel
+            // 
+            this.tbxBokTitel.Location = new System.Drawing.Point(83, 34);
+            this.tbxBokTitel.Name = "tbxBokTitel";
+            this.tbxBokTitel.Size = new System.Drawing.Size(100, 20);
+            this.tbxBokTitel.TabIndex = 1;
+            // 
+            // btnBok
+            // 
+            this.btnBok.Location = new System.Drawing.Point(45, 121);
+            this.btnBok.Name = "btnBok";
+            this.btnBok.Size = new System.Drawing.Size(138, 23);
+            this.btnBok.TabIndex = 2;
+            this.btnBok.Text = "registrera bok";
+            this.btnBok.UseVisualStyleBackColor = true;
+            // 
+            // lbxList
+            // 
+            this.lbxList.FormattingEnabled = true;
+            this.lbxList.Location = new System.Drawing.Point(43, 235);
+            this.lbxList.Name = "lbxList";
+            this.lbxList.Size = new System.Drawing.Size(729, 186);
+            this.lbxList.TabIndex = 2;
+            // 
+            // tbxBokSidor
+            // 
+            this.tbxBokSidor.Location = new System.Drawing.Point(83, 70);
+            this.tbxBokSidor.Name = "tbxBokSidor";
+            this.tbxBokSidor.Size = new System.Drawing.Size(100, 20);
+            this.tbxBokSidor.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "antal sidor";
+            // 
+            // tbxLjudSpeltid
+            // 
+            this.tbxLjudSpeltid.Location = new System.Drawing.Point(91, 70);
+            this.tbxLjudSpeltid.Name = "tbxLjudSpeltid";
+            this.tbxLjudSpeltid.Size = new System.Drawing.Size(100, 20);
+            this.tbxLjudSpeltid.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "speltid";
+            // 
+            // btnLjudspår
+            // 
+            this.btnLjudspår.Location = new System.Drawing.Point(44, 121);
+            this.btnLjudspår.Name = "btnLjudspår";
+            this.btnLjudspår.Size = new System.Drawing.Size(147, 23);
+            this.btnLjudspår.TabIndex = 7;
+            this.btnLjudspår.Text = "registrera ljudspår";
+            this.btnLjudspår.UseVisualStyleBackColor = true;
+            // 
+            // tbxLjudTitel
+            // 
+            this.tbxLjudTitel.Location = new System.Drawing.Point(91, 34);
+            this.tbxLjudTitel.Name = "tbxLjudTitel";
+            this.tbxLjudTitel.Size = new System.Drawing.Size(100, 20);
+            this.tbxLjudTitel.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "titel";
+            // 
+            // tbxFilmSpeltid
+            // 
+            this.tbxFilmSpeltid.Location = new System.Drawing.Point(85, 66);
+            this.tbxFilmSpeltid.Name = "tbxFilmSpeltid";
+            this.tbxFilmSpeltid.Size = new System.Drawing.Size(100, 20);
+            this.tbxFilmSpeltid.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "speltid";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(67, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "registrera film";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // tbxFilmTitel
+            // 
+            this.tbxFilmTitel.Location = new System.Drawing.Point(85, 34);
+            this.tbxFilmTitel.Name = "tbxFilmTitel";
+            this.tbxFilmTitel.Size = new System.Drawing.Size(100, 20);
+            this.tbxFilmTitel.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "titel";
+            // 
+            // tbxFilmUpplösning
+            // 
+            this.tbxFilmUpplösning.Location = new System.Drawing.Point(85, 95);
+            this.tbxFilmUpplösning.Name = "tbxFilmUpplösning";
+            this.tbxFilmUpplösning.Size = new System.Drawing.Size(100, 20);
+            this.tbxFilmUpplösning.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "upplösning";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxList);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbxBokSidor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBok;
+        private System.Windows.Forms.TextBox tbxBokTitel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbxLjudSpeltid;
+        private System.Windows.Forms.Button btnLjudspår;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxLjudTitel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tbxFilmUpplösning;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbxFilmSpeltid;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxFilmTitel;
+        private System.Windows.Forms.ListBox lbxList;
+    }
+}
+
